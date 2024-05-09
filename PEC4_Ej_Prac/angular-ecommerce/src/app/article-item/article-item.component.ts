651b8a5349f52c5171efd65889efa8de9b16cab5
OmarHeviaArbana <ohevia@uoc.edu>
2024-05-09 17:23:28 +0200

    Exercise 5 finished

--- PEC4_Ej_Prac/angular-ecommerce/src/app/article-item/article-item.component.ts
+++ PEC4_Ej_Prac/angular-ecommerce/src/app/article-item/article-item.component.ts
@@ -13,7 +13,7 @@ export class ArticleItemComponent {
     name: 'MacBook Pro de 14 pulgadas - Gris espacial',
     imageUrl: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-14-spacegray-cto-hero-202310?wid=772&hei=466&fmt=jpeg&qlt=90&.v=1697913381764',
     price: 2.029,
-    isOnSale: false,
+    isOnSale: true,
     quantityInCart: 0
   };
 
